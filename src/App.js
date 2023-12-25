13a4f9274c13d7928096496f68645c878601cfc7
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2023-12-25 11:35:02 +0700

    Add style.css & update App.js External css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import './style.css'
 function App(){
-let r ='React'
-let rn ='React Native'
-const el =(
-  <div>
-    Create Web & Mobile Apps<br/>
-    with {r}&{rn}
-  </div>
-)
-return el 
+  return(
+    <>
+    <h2>DevelopThai.com</h2>
+    <div className="title">React & React Native</div>
+    </>
+  )
 }
 export default App;
