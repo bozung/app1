5ef9d28e4106b25e0d7fa2c0d3f829544a89d548
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2024-01-29 12:10:48 +0700

    State with Context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { userContext } from "./compoents/context";
-import Content2 from "./compoents/context-content2";
-import Header2 from "./compoents/context-header2";
+import { userContext } from "./context";
+import Header2 from "./context-header2";
+import Content2 from "./context-content2";
 
 function App() {
   let [user, setUser] = React.useState("");
   return (
-    <>
-      <userContext.Provider value={[user, setUser]}>
-        <Header2 />
-        <Content2 />
-      </userContext.Provider>
-    </>
+    <userContext.Provider value={[user, setUser]}>
+      <Header2 />
+      <Content2 />
+    </userContext.Provider>
   );
 }
 
