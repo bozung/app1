b5fa3f7ae4dbb815bc46458e68d5fc6d250a64a2
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2024-02-16 22:00:09 +0700

    Router 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from "react";
-import { userContext } from "./context";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
-
-function App() {
-  let [user, setUser] = React.useState("");
-  return (
-    <userContext.Provider value={[user, setUser]}>
-      <Header2 />
-      <Content2 />
-    </userContext.Provider>
-  );
+import Rounter2 from "./router2";
+export default function App() {
+  return <Rounter2 />;
 }
-
-export default App;
